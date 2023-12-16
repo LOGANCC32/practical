db66e5477d009fa4d1e5c6b1c77ec0e96a914715
Toson <kareemtoson1@gmail.com>
2023-12-16 18:38:16 +0200

    sum fun in master

--- main.cpp
+++ main.cpp
@@ -5,4 +5,9 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, from math!" << std::endl;
     return 0;
+    int   int sum(int a, int b,int x)
+    {
+
+        return a, b,x;
+    }
 }
